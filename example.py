2e7aa48c5b602052f41e477b816c4b57473246d3
NoixD <henrique.figueira.lopes@gmail.com>
2015-12-05 21:35:16 +0000

    add new file

---
+++ example.py
@@ -0,0 +1,33 @@
+import humm
+from flask import Flask, request, redirect
+import twilio.twiml
+import json
+app = Flask(__name__)
+
+
+@app.route("/", methods = ['GET', 'POST'])
+
+def hello_monkey():
+	msg_body = request.values.get('Body', None)
+	msg_from = request.values.get('From', None)
+
+	client_id = '5663510cae8c502d4f8b456a'
+	client_secret = 'ba9488a4ea2efcc0fef6d4ba71888ad29e8cfd616c94db77a47343879fccedab'
+	grant_type = 'client_credentials'
+
+
+	at = humm.find_authorzation_token
+
+	top_songs = top_songs_request(at, msg_body)
+
+	resp = twilio.twiml.Response()
+
+	if(top_songs == 'error: artist_not_found'):
+		resp.message("Artist not in repository")
+
+	elif(top_songs == 'error: top list not found'):
+		resp.message("Top songs not available")
+	else: 
+		return jsonify(top_songs)
+
+	return jsonify({})
