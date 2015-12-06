3097102003b0314c7fb5b1d6495e517d8ed84a9e
amsihralhan <amish.jimmy@gmail.com>
2015-12-06 04:46:29 +0000

    reduce search length to 4

--- humm2_test.py
+++ humm2_test.py
@@ -13,7 +13,7 @@ def find_authorzation_token(client_id,client_secret,grant_type):
     return response.body['data_response']['access_token']
 
 def find_artist_id(authorzation_token,artist):
-    string="https://humm-api.p.mashape.com/artists?auth="+authorzation_token+"&keyword="+artist+'&limit=4'
+    string="https://humm-api.p.mashape.com/artists?auth="+authorzation_token+"&keyword="+artist
     response = unirest.get(string,
                            headers={
                                     "X-Mashape-Key": "m78ha7jd57msh8cdt9qLh4maGu8dp1DhHlljsnzcpHjUoYYnLJ",
@@ -25,7 +25,7 @@ def find_artist_id(authorzation_token,artist):
         return response.body['data_response'][0]['_id']
 
 def find_top_songs(authorzation_token,artist_id):
-    string='https://humm-api.p.mashape.com/artists/'+artist_id+'/topsongs?auth='+authorzation_token+'&limit=4'
+    string='https://humm-api.p.mashape.com/artists/'+artist_id+'/topsongs?auth='+authorzation_token
     
     response = unirest.get(string,
                            headers={
@@ -45,19 +45,41 @@ def find_top_songs(authorzation_token,artist_id):
         dic = {first_name: youtube_first_link, second_name: youtube_second_link, third_name: youtube_third_link}
         return dic
 
-        
+
+
+
+def find_genre(authorzation_token,genre):
+    string='https://humm-api.p.mashape.com/songs/popular?auth='+authorzation_token+'&genre='+genre
+    
+    response = unirest.get(string,
+                           headers={
+                                    "X-Mashape-Key": "m78ha7jd57msh8cdt9qLh4maGu8dp1DhHlljsnzcpHjUoYYnLJ",
+                                    "Accept": "application/json"
+                                    })
+
+    if response.code != 200:
+        return 'error: genre not find':
+    else
+        youtube_first_link=response.body['data_response'][0]['foreign_ids']['youtube']
+        first_name=response.body['data_response'][0]['title']
+        youtube_second_link=response.body['data_response'][1]['foreign_ids']['youtube']
+        second_name=response.body['data_response'][1]['title']
+        youtube_third_link=response.body['data_response'][2]['foreign_ids']['youtube']
+        third_name=response.body['data_response'][2]['title']
+        dic = {first_name: youtube_first_link, second_name: youtube_second_link, third_name: youtube_third_link}
+        return dic
 
 def top_songs_request(authorzation_token,message):
     #function that returns the top songs of an artist
     #try:
     artist_id=find_artist_id(authorzation_token,message)
-    if artist_id != 'error: artist_not_found':
+    if artist_id != 'artist_not_found':
         top_songs = find_top_songs(authorzation_token,artist_id)
+        print "top_songs: %s" % top_songs
+        return top_songs
 
         if top_songs == 'error: top list not found':
             return 'error: top list not found'
-        else:
-            return top_songs
     else:
         return 'error: artist_not_found'
     #except Exception, e:
